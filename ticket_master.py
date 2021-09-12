2cdbb8b07d29e299aa6582652d98e5b63ff2d027
szydab1 <up814818@myport.ac.uk>
2021-09-12 13:01:08 +0100

    Adds ability to buy tickets and shows total cost

--- ticket_master.py
+++ ticket_master.py
@@ -3,3 +3,9 @@ TICKET_PRICE = 10
 tickets_remaining = 100
 
 print("There are {} tickets remaining".format(tickets_remaining))
+
+user_name = str(input("What is your name? "))
+num_tickets = int(input("{}, how many tickets would you like to purchase? "
+    .format(user_name)))
+total_due = num_tickets * TICKET_PRICE
+print("Total price due is ${}".format(total_due))
