32cab67b2267288be89b1e307753d3c0493ba75d
szydab1 <up814818@myport.ac.uk>
2021-09-12 13:22:22 +0100

    Adds ability to confirm order

--- ticket_master.py
+++ ticket_master.py
@@ -9,3 +9,11 @@ num_tickets = int(input("{}, how many tickets would you like to purchase? "
     .format(user_name)))
 total_due = num_tickets * TICKET_PRICE
 print("Total price due is ${}".format(total_due))
+permission = str(input("Would you like to proceed {}? (Y/N): "
+    .format(user_name)))
+if permission.lower() == "y":
+    #TODO: Gather credit card infromatio nand process it.
+    print("SOLD!")
+    tickets_remaining -= num_tickets
+elif permission.lower() == "n":
+    print("Thank you for your interest {}".format(user_name))
